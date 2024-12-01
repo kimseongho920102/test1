a98156e3f357158763a1901f0c8e6f17d005f2b5
seong_ho_kim <92960654+kimseongho920102@users.noreply.github.com>
2024-12-01 12:33:38 +0000

    spring boot

---
+++ backend/src/main/java/com/example/demo/WebConfig.java
@@ -0,0 +1,7 @@
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+    }
+}
