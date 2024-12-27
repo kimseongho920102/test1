92ecb91ad05f883c897055ecebc933259b3cc2fd
seong_ho_kim <92960654+kimseongho920102@users.noreply.github.com>
2024-12-27 07:13:39 +0000

    push

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# 1. React 빌드 및 파일 복사
-cd ~/test1/my-app
-npm install
-npm run build
-cp -r build/* ../backend/src/main/resources/static/
-
-# 2. Spring Boot 빌드 및 시스템 서비스 재시작
-cd ~/test1/backend
-./gradlew clean build --refresh-dependencies
-
 # 서비스 재시작
 sudo systemctl restart backend.service
 
--- gitpush.sh
+++ gitpush.sh
@@ -1,3 +1,13 @@
+# 1. React 빌드 및 파일 복사
+cd /workspace/test1/my-app
+npm install
+npm run build
+cp -r build/* ../backend/src/main/resources/static/
+
+# 2. Spring Boot 빌드 및 시스템 서비스 재시작
+cd /workspace/test1/backend
+./gradlew clean build --refresh-dependencies
+
 git add .
 git commit -m 'push'
 git push origin main
