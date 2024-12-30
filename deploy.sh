512e142043e7633259b667950d5ec54c3a00f85e
seong_ho_kim <92960654+kimseongho920102@users.noreply.github.com>
2024-12-30 03:48:43 +0000

    자동화 테스트

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+cd ~/test1/my-app
+
+npm run build
+cp -r build/* ../backend/src/main/resources/static/
+
+# 2. Spring Boot 빌드 및 시스템 서비스 재시작
+cd ~/test1/backend
+./gradlew clean build --refresh-dependencies
+
 # 서비스 중지
 sudo systemctl stop backend.service
 
